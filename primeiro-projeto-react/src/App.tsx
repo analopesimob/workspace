63c2e69f5d16930632dc361b5141aa170f2adb2b
Ana Lopes <carol.llopes@hotmail.com>
2025-02-07 11:40:17 -0300

    add: update carousel component to accept dynamic images and styles, and modify app layout colors

--- primeiro-projeto-react/src/App.tsx
+++ primeiro-projeto-react/src/App.tsx
@@ -20,7 +20,7 @@ import viteLogo from '/vite.svg';
 const App = () => {
     return (
         <>
-            <div className='flex min-h-screen flex-col items-center justify-center gap-3 bg-orange-500 text-center'>
+            <div className='flex min-h-screen flex-col items-center justify-center gap-3 bg-slate-500 text-center'>
                 <div className='flex items-center justify-center gap-2'>
                     <a href='https://vite.dev' target='_blank'>
                         <img src={viteLogo} className='logo' alt='Vite logo' />
@@ -34,10 +34,31 @@ const App = () => {
                     </a>
                     <span className='text-4xl'>😍</span>
                 </div>
-                <h1 className='text-4xl font-semibold text-orange-950'>
-                    Seu Primeiro Projetinho React
+                <h1 className='text-4xl font-semibold text-slate-950'>
+                    Seu Primeiro Projetinho React orange
                 </h1>
-				<CarouselDemo />
+
+                <div className='flex items-center justify-center gap-96'>
+                    <CarouselDemo
+                        className='bg-slate-900'
+                        images={[
+                            'https://images.unsplash.com/photo-1514316454349-750a7fd3da3a?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                            'https://images.unsplash.com/photo-1546518071-fddcdda7580a?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                            'https://images.unsplash.com/photo-1549275301-c9d60945be6b?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                        ]}
+                    />
+
+                    <CarouselDemo
+                        className='bg-slate-900'
+                        slideClassName='aspect-square'
+                        images={[
+                            'https://images.unsplash.com/photo-1549275301-c9d60945be6b?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                            'https://images.unsplash.com/photo-1546518071-fddcdda7580a?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                            'https://images.unsplash.com/photo-1514316454349-750a7fd3da3a?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                        ]}
+                    />
+                </div>
+
                 <div className='card'>
                     <AlertDialog>
                         <AlertDialogTrigger asChild>
--- primeiro-projeto-react/src/components/carousel-demo.tsx
+++ primeiro-projeto-react/src/components/carousel-demo.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent } from '@/components/ui/card';
+import { cn } from '@/lib/utils';
 import {
     Carousel,
     CarouselContent,
@@ -7,24 +8,44 @@ import {
     CarouselPrevious
 } from '@/components/ui/carousel';
 
-const CarouselDemo = () => {
+const CarouselDemo = ({ 
+	className = 'bg-orange-900',
+	slideClassName = '',
+	images = [
+		'https://images.unsplash.com/photo-1514316454349-750a7fd3da3a?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		'https://images.unsplash.com/photo-1546518071-fddcdda7580a?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+	]
+}: { 
+	className?: string;
+	slideClassName?: string;
+	images?: string[];
+}) => {
     return (
-        <Carousel className='w-full max-w-xs'>
+        <Carousel
+            className={cn(
+                'w-full max-w-xs rounded-3xl px-3 py-4',
+				className
+            )}
+        >
             <CarouselContent>
-                {Array.from({ length: 5 }).map((_, index) => (
-                    <CarouselItem key={index}>
-                        <div className='p-1'>
-                            <Card>
-                                <CardContent className='flex aspect-square items-center justify-center p-6'>
-                                    <span className='text-4xl font-semibold'>
-                                        {index + 1}
-                                    </span>
-                                </CardContent>
-                            </Card>
-                        </div>
-                    </CarouselItem>
-                ))}
+				{images.map((image, index) => {
+					return (
+						<CarouselItem data-slide={index}
+							key={index}>
+							<Card className='overflow-hidden rounded-3xl border-none bg-transparent'>
+								<CardContent
+									className={cn('flex aspect-[9/16] items-center justify-center bg-cover p-6', slideClassName)}
+									style={{
+										backgroundImage: `url(${image})`
+									}}
+								/>
+							</Card>
+						</CarouselItem>
+
+					);
+				})}
             </CarouselContent>
+
             <CarouselPrevious />
             <CarouselNext />
         </Carousel>
