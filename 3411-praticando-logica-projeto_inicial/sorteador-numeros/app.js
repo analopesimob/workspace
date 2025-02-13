3717c57327513c667ed201068cc5cd357dbde9f7
Ana Lopes <carol.llopes@hotmail.com>
2025-02-13 19:40:01 -0300

    cine-Rohde

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -3,6 +3,13 @@ function sortear(){
 	let de = parseInt(document.getElementById('de').value);
 	let ate = parseInt(document.getElementById('ate').value);
 
+	let numero = obterNumeroAleatorio(de, ate);
+	alert(numero);
+
+	function obterNumeroAleatorio(min, max){
+		return Math.floor(Math.ramdom() * (max - min + 1)) + min;
+	}
+
 	alert(`Quantidade: ${quantidade}`);
 	alert(`Do número: ${de}`);	
 	alert(`Até o número: ${ate}`);
